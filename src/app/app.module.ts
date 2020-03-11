04e0ae5bb591e1361c2945c8941264a8f05473f6
CCCCChloe <tpanchloe@gmail.com>
2020-03-11 13:45:30 -0500

    todo list, add todo, give priority

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { from } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatRadioModule,
+  ], 
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todos';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+  animations : [
+    // Here we are defining what are the states our panel can be in 
+    // and the style each state corresponds to.
+    trigger('panelState', [
+      state('closed', style({ height: '32px', overflow: 'hidden' })),
+      state('open', style({ height: '*' })),
+      transition('closed <=> open', animate('300ms ease-in-out')),
+    ]),
+  ],
+})
+export class TodosComponent implements OnInit {
+  //todos = TODOS;
+  todos: Todo[];
+  selectedTodo: Todo;
+  todoTitle: string;
+  todoID: number;
+  titleCache: string;
+
+  folded = 'closed';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todoID = 4;
+    this.todoTitle = '';
+    this.todos = [
+      {
+        'id': 1,
+        'title': 'task #1',
+        'completed': false,
+        'priority': '',
+        'editing': false,
+      },
+      {
+        'id': 2,
+        'title': 'task #2',
+        'completed': false,
+        'priority': '',
+        'editing': false,
+      },
+      {
+        'id': 3,
+        'title': 'task #3',
+        'completed': false,
+        'priority': '',
+        'editing': false,
+      },
+    ]
+  }
+
+  addTodo(): void {
+    if (this.todoTitle.trim().length == 0) {
+      return;
+    }
+    this.todos.push(
+      {
+        'id': this.todoID,
+        'title': this.todoTitle,
+        'completed': false,
+        'priority': '',
+        'editing': false,
+      }
+    )
+    this.todoTitle = '';
+    this.todoID++;
+  }
+
+  onSelect(todo: Todo): void {
+    this.selectedTodo = todo;
+  }
+
+  deleteTodo(id: number): void {
+    this.todos = this.todos.filter(todo => todo.id != id);
+  }
+
+  editTodo(todo: Todo): void {
+    this.titleCache = todo.title;
+    todo.editing = true;
+  }
+
+  doneEdit(todo: Todo): void {
+    if (todo.title.trim().length == 0) {
+      todo.title = this.titleCache;
+    }
+    todo.editing = false;
+  }
+
+  cancelEdit(todo: Todo): void {
+    todo.title = this.titleCache;
+    todo.editing = false;
+  }
+
+  remaining(): number {
+    return this.todos.filter(todo => todo.completed == false).length;
+  }
+
+  toggleFold(){
+    this.folded = this.folded === 'open' ? 'closed' : 'open';
+  }
+}
